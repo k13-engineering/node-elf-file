916c2e18b7d284d4c08af13b6e5372e1bdceffed
Simon Kadisch <simon.kadisch@k13-engineering.at>
2019-04-28 20:10:06 +0200

    Initial implementation of layouting functionality

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-global-assign */
 require = require("esm")(module);
 const elf = require("./lib/elf.js").default;
+const layouter = require("./lib/layout.js").default;
 
 const elfFile = {
   "parse": elf.parse,
   "format": elf.format,
-  "validate": elf.validate
+  "validate": elf.validate,
+
+  "layout": layouter.layout,
+  "delayout": layouter.delayout
 };
 
 module.exports = elfFile;
---
+++ test/layout.js
@@ -0,0 +1,50 @@
+import assert from "assert";
+import elf from "../index.js";
+import assets from "./assets.js";
+
+describe("layouting", function () {
+  assets.all.forEach(({ name, data }) => {
+    describe(name, () => {
+      it("should delayout without error", () => {
+        elf.delayout(elf.parse(data));
+      });
+
+      it("should relayout without error", () => {
+        const parsed = elf.parse(data);
+        const layouted = elf.layout(parsed);
+        elf.validate(layouted);
+      });
+
+      it("should delayout and layout without error", () => {
+        const parsed = elf.parse(data);
+        const delayouted = elf.delayout(parsed);
+        const layouted = elf.layout(delayouted);
+        elf.validate(layouted);
+      });
+
+      it("should only touch layout specific data", () => {
+        const parsed = elf.parse(data);
+        const before = elf.delayout(parsed);
+        const after = elf.delayout(elf.layout(before));
+
+        assert.deepEqual(after, before);
+      });
+
+      it("should show a reproducible behaviour when layouting using same input", () => {
+        const input = elf.delayout(elf.parse(data));
+        const output1 = elf.layout(input);
+        const output2 = elf.layout(input);
+
+        assert.deepEqual(output2, output1);
+      });
+
+      it("should show a reproducible behaviour when layouting using chain", () => {
+        const input = elf.delayout(elf.parse(data));
+        const output1 = elf.layout(input);
+        const output2 = elf.layout(elf.delayout(output1));
+
+        assert.deepEqual(output2, output1);
+      });
+    });
+  });
+});
